0ebb1cb93ca1e770b4d9dd9ae8fa81ca33254c6b
the one and only <133546210+thezahrand@users.noreply.github.com>
2024-03-05 19:31:47 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "thezahrand",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "Bersama Lebih Baik",
+    email: "theonehzx@gmail.com",
+    linkedin: "theonehzx",
+    github: "thezahrand",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `BijakAsuh`,
+      href: "https://github.com/thezahrand/bijakasuh.co.id",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Bijak Mengasuh",
+    description: "Selamat datang!",
   },
 
   // CONFIG configration (required)
